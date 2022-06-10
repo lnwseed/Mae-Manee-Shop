0b4e7f9c659ffefa54a3cff45324ef357ea6dcbb
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2022-06-10 21:17:15 +0700

    Update class.Maemanee.php

--- class.Maemanee.php
+++ class.Maemanee.php
@@ -1,93 +1,116 @@
-<?php	
-    class MaeManee { 
-        private $Idard;
-        private $Passkey;		
-		public $curl_options = array(
-			CURLOPT_SSL_VERIFYPEER => false //lnwseed
-		);
-		public $api_gateway = "https://manee.3ird.online/Gateway/";
-	    	public $api_gatewuy = "https://merchantbff.scb.co.th/";
-        public function __construct($Idard = null, $Passkey = null) {
-            $this->Idard = $Idard;
-            $this->Passkey = $Passkey;
-		}
-		public function request ($api_path, $headers = array(), $data = null) {
-			$this->data = null;
-			$handle = curl_init($this->api_gateway.ltrim($api_path, "/"));
-			if (!is_null($data)) {
-				curl_setopt_array($handle, array(
-				CURLOPT_POST => true,
-				CURLOPT_POSTFIELDS => is_array($data) ? json_encode($data) : $data
-				));
-				if (is_array($data)) $headers = array_merge(array("Content-Type" => "application/json"), $headers);
-			}
-			curl_setopt_array($handle, array(
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_USERAGENT => "okhttp/3.8.0",
-			CURLOPT_HTTPHEADER => $this->buildHeaders($headers)
-			));
-			if (is_array($this->curl_options)) curl_setopt_array($handle, $this->curl_options);
-			$this->response = curl_exec($handle);
-			$this->http_code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-			if ($result = json_decode($this->response, true)) {
-			//if ($result = $this->response) {
-				if (isset($result["data"])) $this->data = $result["data"];
-				return $result;
-			}
-			return $this->response;
-		}	
-		public function buildHeaders ($array) {
-			$headers = array();
-			foreach ($array as $key => $value) {
-				$headers[] = $key.": ".$value;
-			}
-			return $headers;
-		}	
+<?php
+class MaeManee
+{
+    private $idcard;
+    private $pin;
+    private $phone;
+    public $curl_options = [
+        CURLOPT_SSL_VERIFYPEER => true,
+    ];
+    public $api_gateway = "https://maemanee.truewallet.me/api/sys_manee.php";
+    public function __construct(
+        $manee_card = null,
+        $mamee_pin = null,
+        $manee_phone = null
+    ) {
+        $this->idcard = $manee_card;
+        $this->pin = $mamee_pin;
+        $this->phone = $manee_phone;
+    }
+    public function Connect($data = null)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $this->api_gateway,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_SSL_VERIFYPEER => false,
+        ]);
+        $response = curl_exec($curl);
+        $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($http_code == 200 && ($result = json_decode($response, true))) {
+            return $result;
+        }
+    }
 
-		public function GetSignin() {
-			if (is_null($this->Passkey) || is_null($this->Idard)) return false; 
-			return $this->request("GetSignin/".$this->Idard);
-		}	
-		
-		public function RequestLoginOTP() {
-			if (is_null($this->Passkey) || is_null($this->Idard)) return false; 
-			return $this->request("OTPGet/".$this->Idard);
-		}	
+    public function GetQrcode(
+        $bill_id,
+        $bill_ref1,
+        $bill_ref3,
+        $orderId,
+        $amount
+    ) {
+        $postdata = [];
+        $postdata["idcard"] = $this->idcard;
+        $postdata["pin"] = $this->pin;
+        $postdata["type"] = "Genqrcode";
+        $postdata["qrbill_id"] = $bill_id;
+        $postdata["qrbill_ref1"] = $bill_ref1;
+        $postdata["qrbill_ref3"] = $bill_ref3;
+        $postdata["qrbill_orderId"] = $orderId;
+        $postdata["qrbill_amount"] = $amount;
+        return $this->Connect($postdata);
+    }
 
-		public function SubmitLoginOTP($otp) {
-			if (is_null($this->Passkey) || is_null($this->Idard) || is_null($otp)) return false; 
-			return $this->request("OPTSubmit/".$this->Idard."/".$otp);
-		}	
+    public function RequestLoginOTP()
+    {
+        $postdata = [];
+        $postdata["idcard"] = $this->idcard;
+        $postdata["pin"] = $this->pin;
+        $postdata["type"] = "OTPGet";
+        $postdata["phone"] = $this->phone;
+        return $this->Connect($postdata);
+    }
 
-		public function CheckToken() {
-			if (is_null($this->Passkey) || is_null($this->Idard)) return false; 
-			return $this->request("CheckToken/".$this->Idard);
-		}	
+    public function SubmitLoginOTP($otp_ref)
+    {
+        $postdata = [];
+        $postdata["idcard"] = $this->idcard;
+        $postdata["pin"] = $this->pin;
+        $postdata["type"] = "OPTSubmit";
+        $postdata["otp_ref"] = $otp_ref;
+        return $this->Connect($postdata);
+    }
 
-		public function RefreshToken() {
-			if (is_null($this->Passkey) || is_null($this->Idard)) return false; 
-			return $this->request("RefreshToken/".$this->Idard);
-		}					
-						
-		public function Logout() {
-			if (is_null($this->Passkey) || is_null($this->Idard)) return false; 
-			return $this->request("Logout/".$this->Idard);
-		}
-		
-		public function Transaction() {
-			if (is_null($this->Passkey) || is_null($this->Idard)) return false; 
-			return $this->request("BillTransaction/".$this->Idard);
-		}
+    public function GetBalance()
+    {
+        $postdata = [];
+        $postdata["idcard"] = $this->idcard;
+        $postdata["pin"] = $this->pin;
+        $postdata["type"] = "Balance";
+        return $this->Connect($postdata);
+    }
 
-		public function Create($ref, $amount) {
-			if (is_null($this->Passkey) || is_null($this->Idard) || is_null($ref) || is_null($amount)) return false; 
-			return $this->request("BillCreate/".$this->Idard."/".$ref."/".$amount);
-		}		
+    public function GetCreate($user_ref, $amount)
+    {
+        $postdata = [];
+        $postdata["idcard"] = $this->idcard;
+        $postdata["pin"] = $this->pin;
+        $postdata["type"] = "BillCreate";
+        $postdata["bill_ref"] = $user_ref;
+        $postdata["bill_amount"] = $amount;
+        return $this->Connect($postdata);
+    }
 
-		public function ImgCode($orderId, $amount) {
-			if (is_null($this->Passkey) || is_null($this->Idard) || is_null($orderId) || is_null($amount))) return false; 
-			return $this->request("Qrgen/".$this->Idard."/".$orderId."/".$amount);
-		}											
-	}
+    public function GetTransaction()
+    {
+        $postdata = [];
+        $postdata["idcard"] = $this->idcard;
+        $postdata["pin"] = $this->pin;
+        $postdata["type"] = "BillTransaction";
+        return $this->Connect($postdata);
+    }
 
-?>
+    public function GetSuccessill()
+    {
+        $postdata = [];
+        $postdata["idcard"] = $this->idcard;
+        $postdata["pin"] = $this->pin;
+        $postdata["type"] = "SuccessBill";
+        return $this->Connect($postdata);
+    }
+}
+?> 
